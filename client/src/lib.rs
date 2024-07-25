657b09e9fa8ec87b74cf509baa98e464cbb1039f
Roman Makarevich <remmybump@gmail.com>
2024-07-25 16:27:10 +0300

    hack deserialize hex

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -27,8 +27,7 @@ pub extern crate jsonrpc;
 pub extern crate bitcoincore_rpc_json;
 pub use crate::json::bitcoin;
 pub use bitcoincore_rpc_json as json;
-use json::bitcoin::consensus::{Decodable, ReadExt};
-use json::bitcoin::hex::HexToBytesIter;
+use json::bitcoin::consensus::{Decodable};
 
 mod client;
 mod error;
@@ -39,13 +38,5 @@ pub use crate::error::Error;
 pub use crate::queryable::*;
 
 fn deserialize_hex<T: Decodable>(hex: &str) -> Result<T> {
-    let mut reader = HexToBytesIter::new(&hex)?;
-    let object = Decodable::consensus_decode(&mut reader)?;
-    if reader.read_u8().is_ok() {
-        Err(Error::BitcoinSerialization(bitcoin::consensus::encode::Error::ParseFailed(
-            "data not consumed entirely when explicitly deserializing",
-        )))
-    } else {
-        Ok(object)
-    }
+    Ok(bitcoin::consensus::encode::deserialize_hex(hex).unwrap())
 }
